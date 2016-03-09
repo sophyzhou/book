f796f079dcade33e35a7412b06fcc75d34c7eb22
Caleb Hsu <chialo.hsu@colorado.edu>
2016-03-09 16:22:30 -0700

    Added navbar components and home page

--- src/apps/together/landing/components/navbar.jsx
+++ src/apps/together/landing/components/navbar.jsx
@@ -1,1 +1,17 @@
-navbar.jsx
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper cyan darken-3">
+          <ul id="nav-mobile" className="left">
+            <li><a href="/">Home</a></li>
+          </ul>
+          <a href="index.html" className="brand-logo center">Prolanner</a>
+          <ul id="nav-mobile" className="right">
+            <li><a href="/apps/together/landing/">Login</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
