f06455881a6c565e742bde1be650a3a5f7fef61f
PeicheTsao <peichetsao@gmail.com>
2016-03-12 18:47:22 -0700

    commit for pull

---
+++ src/apps/together/project/js/chatroom.js
@@ -0,0 +1,25 @@
+/**
+ * Created by pepe on 3/12/16.
+ */
+(function(){
+
+
+    var searchFilter = {
+        options: { valueNames: ['name'] },
+        init: function() {
+            var userList = new List('people-list', this.options);
+            var noItems = $('<li id="no-items-found">No items found</li>');
+
+            userList.on('updated', function(list) {
+                if (list.matchingItems.length === 0) {
+                    $(list.list).append(noItems);
+                } else {
+                    noItems.detach();
+                }
+            });
+        }
+    };
+
+    searchFilter.init();
+
+})();
