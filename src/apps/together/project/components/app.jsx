8577a1a3d2f726a403996fdd25f700d841431c4b
Kavya Ravikumar <kara8075@colorado.edu>
2016-03-14 17:43:55 -0600

    Tasklist working

--- src/apps/together/project/components/app.jsx
+++ src/apps/together/project/components/app.jsx
@@ -1,1 +1,14 @@
-app.jsx
+class App extends React.Component {
+  render(){
+  	console.log("task in app:")
+    console.log(this.props.data.tasks)
+    return(
+    <div>
+      <MyComponents.TaskList tasks={this.props.data.tasks}/>
+      
+    </div>
+    );
+  }
+}
+
+MyComponents.App = App
