e6bfcf51b29876b903969424188a68409d8479e0
Caleb Hsu <chialo.hsu@colorado.edu>
2016-03-15 12:45:04 -0600

    Did stuff to logo font

--- src/apps/together/project/components/navbar.jsx
+++ src/apps/together/project/components/navbar.jsx
@@ -3,7 +3,7 @@ class NavBar extends React.Component {
     return (
       <nav>
         <div className="nav-wrapper cyan darken-3">
-          <a href="index.html" className="brand-logo center">Prolanner</a>
+          <a href="index.html" className="brand-logo center" id="logo">Prolanner</a>
           <ul id="nav-mobile" className="right">
             <li><a href="/apps/together/landing/">Landing</a></li>
           </ul>
