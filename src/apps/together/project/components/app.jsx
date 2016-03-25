5693cb54b1e8683af505439b5faeecfe12b8838f
lilyJi <lili.ji@colorado.edu>
2016-03-24 18:28:57 -0600

    Add event column

--- src/apps/together/project/components/app.jsx
+++ src/apps/together/project/components/app.jsx
@@ -2,8 +2,15 @@ class App extends React.Component {
   render(){
     return(
     <div>
+    <div>
+      <MyComponents.EventList events={this.props.data.events}/>
+    </div>
+    <div>
       <MyComponents.TaskList tasks={this.props.data.tasks}/>
     </div>
+    </div>
+
+
     );
   }
 }
