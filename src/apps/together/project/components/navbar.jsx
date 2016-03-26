14c2c84304099ba020155b2a8786f0e2c0f805e2
lilyJi <lili.ji@colorado.edu>
2016-03-25 21:09:20 -0600

    Add events

--- src/apps/together/project/components/navbar.jsx
+++ src/apps/together/project/components/navbar.jsx
@@ -6,6 +6,8 @@ class NavBar extends React.Component {
           <a href="/apps/together/" className="brand-logo center" id="logo">Prolanner</a>
           <ul id="nav-mobile" className="right">
             <li><a href="/apps/together/landing/">Landing</a></li>
+            <li><a href="/apps/together/project/inbox.html">Add new event</a></li>
+
           </ul>
         </div>
       </nav>
---
+++ src/apps/together/project/js/saveNewEventData.js
@@ -0,0 +1,22 @@
+var prolannerRef = new Firebase('https://prolanner.firebaseio.com')
+
+
+function saveData() {
+
+	var eventDate = Math.floor(Date.now() / 1000)
+	var eventName = document.getElementById("event_name").value;
+  var projectID = window.location.hash.substring(1);
+	var newEventRef = prolannerRef.child('events').push()
+
+	var events = {
+		eventDate: Math.floor(Date.now() / 1000),
+		eventName: eventName,
+		location : " "
+	}
+
+
+	newEventRef.set(events)
+
+	console.log(newEventRef.key())
+
+}
