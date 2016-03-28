e01c36c26796bc25b49f3d41e6aa34eb9ff01dda
Caleb Hsu <chialo.hsu@colorado.edu>
2016-03-27 18:42:42 -0600

    Updated working task form

--- src/apps/together/project/js/saveNewTask.js
+++ src/apps/together/project/js/saveNewTask.js
@@ -2,25 +2,24 @@ var prolannerRef = new Firebase('https://prolanner.firebaseio.com')
 
 
 function saveData() {
-
-	var priority = "0"
-	var taskDescription = "New task"
-	var taskStatus = "2"
-	var taskDate = Math.floor(Date.now() / 1000)
-	var taskName = document.getElementById("task_name").value;
+  var priority = document.getElementById("priority").value;
+  var taskDescription = document.getElementById("task_desc").value
+  var taskStatus = "0"
+  var taskDate = Math.floor(Date.now() / 1000)
+  var taskName = document.getElementById("task_name").value;
   var projectID = window.location.hash.substring(1);
-	var newEventRef = prolannerRef.child('tasks').child(projectID).push()
+  var newEventRef = prolannerRef.child('tasks').child(projectID).push()
 
-	var tasks = {
-		assignedTo: " ",
-		deadline: Math.floor(Date.now() / 1000),
-		priority: priority,
-		taskDescription: taskDescription,
-		taskName: taskName,
-		taskStatus:"0"
-	}
+  var tasks = {
+    assignedTo: " ",
+    deadline: Math.floor(Date.now() / 1000),
+    priority: priority,
+    taskDescription: taskDescription,
+    taskName: taskName,
+    taskStatus:taskStatus
+  }
 
   newEventRef.set(tasks)
-	console.log(projectID)
+  console.log(projectID)
 
 }
